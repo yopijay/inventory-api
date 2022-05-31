533cbb4d49644096f502a2d2d02eb11082647796
Kuya Pijay <53463564+yopijay@users.noreply.github.com>
2022-05-31 14:49:11 +0800

    - trial and error

--- queries.js
+++ queries.js
@@ -1,14 +1,10 @@
 const Pool = require('pg').Pool;
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: {
-        rejectUnauthorized: true
-    }
-    // user: 'pkvlqsfxmtlcii',
-    // host: 'ec2-3-234-131-8.compute-1.amazonaws.com',
-    // database: 'ddt6n0mj96ovss',
-    // password: '45a131b713a6815084a0cba2e9ec231ada5991b3ad755271852f7749500248dc',
-    // port: 5432
+    user: 'pijay',
+    host: 'localhost',
+    database: 'inventory',
+    password: 'root',
+    port: 5432
 });
 
 const getAll = (tbl) => {
